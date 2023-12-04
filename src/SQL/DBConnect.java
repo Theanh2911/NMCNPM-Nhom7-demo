1eb0e52e929f90c895da5cbbc6fdbe08d82b688b
Theanh2911 <89338550+Theanh2911@users.noreply.github.com>
2023-12-04 19:26:35 +0700

    Add files via upload

---
+++ src/SQL/DBConnect.java
@@ -0,0 +1,24 @@
+package SQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+    public static Connection getConnection(){
+        try {
+            Connection cons = null;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cons = DriverManager.getConnection("jdbc:mysql://localhost:3306/quanlydancu", "root", "theanh2911");
+            return cons;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    public static void main(String[] args) throws SQLException {
+        Connection c = getConnection();
+        System.out.println(c.toString());
+        c.close();
+    }
+}
